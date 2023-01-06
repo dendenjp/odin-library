466a1bf622df4e4a6bcde5521db0f25040665c56
dendenjp <daimlerjp@gmail.com>
2023-01-06 04:13:31 -0600

    completed UI

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let myLibrary = [];
+
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
